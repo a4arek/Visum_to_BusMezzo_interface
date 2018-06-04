ab015f1b30d77dcc0c6a0a01f7f2c1d0cf38df2e
Rafal Kucharski <rkucharski@pk.edu.pl>
2018-06-04 21:41:21 +0200

    main call

--- main.py
+++ main.py
@@ -236,18 +236,16 @@ def main(Visum):
 
 if __name__ == "__main__":
     # initialize VISUM and load the .ver file
-    try:
-        # from Visum
-        Visum
-        MAIN_PATH = MAIN_PATH = Visum.GetPath(2)
-    except:
+    if 'Visum' in globals():
+        MAIN_PATH = Visum.GetPath(2)
+    else:
         # standalone
         MAIN_PATH = os.getcwd()
         #MAIN_PATH = "E:\BM"
         MAIN_PATH = MAIN_PATH+"\\test\\Krakow_test_I_obw\\"
         Visum = win32com.client.Dispatch('Visum.Visum')
         Visum.LoadVersion(MAIN_PATH+"KRK_test1.ver")
-        Visum.SetPath(2,MAIN_PATH)
+        Visum.SetPath(2, MAIN_PATH)
     Visum.Graphic.StopDrawing = True
     main(Visum)
     Visum.Graphic.StopDrawing = False
